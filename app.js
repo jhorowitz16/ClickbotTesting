4a04ea4dc6a382eb16f606095e023535556cbf86
Josh Horowitz <joshua.horowitz@tripping.com>
2018-02-14 23:36:46 -0800

    add c for click, g for get, and numbers for shopping

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const TwitchBot = require('twitch-bot')
 const robot = require("robotjs");
+const isNumeric = require("isnumeric");
+
 
 
 const Bot = new TwitchBot({
@@ -8,18 +10,42 @@ const Bot = new TwitchBot({
     channels: ['chatplayslive']
 })
 
+
+
+const COOKIE_X = 250;
+const COOKIE_Y = 470;
+
+const STORE_X = 1550;
+const STORE_BASE_Y = 300;
+const STORE_INCREMENT = 65;
+
+
+robot.moveMouse(COOKIE_X, COOKIE_Y);
+
 Bot.on('join', () => {
 
     Bot.on('message', chatter => {
         const name = chatter.username;
         const msg = chatter.message;
+        const cmd = chatter.message.charAt(0);
         const toSend = 'hello ' + name + '. ' + msg;
         Bot.say(toSend);
         console.log(toSend);
 
-        robot.typeString(toSend +'\r');
-        // robot.moveMouse(100, 100);
-
+        if (cmd === 'c') {
+            console.log('click');
+            robot.moveMouse(COOKIE_X, COOKIE_Y);
+            robot.mouseClick();
+            Bot.say('lemme click');
+        } else if (cmd === 'g') {
+            console.log('get position');
+            const pos = robot.getMousePos();
+            console.log('position: ' + pos.x + ', ' + pos.y);
+        } else if (isNumeric( cmd )) {
+            console.log('spot' + cmd);
+            robot.moveMouse(STORE_X, STORE_BASE_Y + (cmd - 1) * STORE_INCREMENT);
+            robot.mouseClick();
+        }
     });
 });
 
